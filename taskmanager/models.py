934d4eba43b264307674639cdbc9b75efe53e263
confar <tentigers@mail.ru>
2018-08-29 01:41:10 +0300

    Update models.py

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -7,14 +7,12 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     name = models.CharField(max_length=140)
     created_date = models.DateField(auto_now=True)
-    due_date = models.DateField(blank=True, null=True, )
+    due_date = models.DateField(blank=True, null=True, db_index=True)
     completed = models.BooleanField(default=False)
-
     created_by = models.ForeignKey(User, related_name='created_by')
     assignee = models.ForeignKey(
         User, blank=True, null=True, related_name='assignee')
-
-    description = models.TextField(blank=False)
+    description = models.TextField()
     priority = models.PositiveIntegerField(default=False)
 
     # Has due date for an instance of this object passed?
@@ -24,7 +22,7 @@ class Task(models.Model):
             return 1
 
     def __str__(self):
-        return str(self.name)
+        return self.name
 
     class Meta:
         ordering = ["-priority"]
@@ -44,6 +42,6 @@ class Message(models.Model):
     author = models.ForeignKey(User)
     email_from = models.EmailField()
     email_to = models.EmailField(default=False)
-    subject = models.TextField(blank=False)
+    subject = models.TextField()
     date = models.DateTimeField(default=datetime.datetime.now)
-    body = models.TextField(blank=False)
+    body = models.TextField()
